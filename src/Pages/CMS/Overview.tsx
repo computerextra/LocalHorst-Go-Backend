57b0fc7f0fc3a6866ecea54bdab2b6ec6271e01f
jayzone91 <register@j4yz0n3.de>
2024-12-09 10:39:00 +0100

    Update Overview.tsx

--- src/Pages/CMS/Overview.tsx
+++ src/Pages/CMS/Overview.tsx
@@ -1,3 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+
 export default function CmsOverview() {
-  return <></>;
+  return (
+    <>
+      <h1>CMS Übersicht</h1>
+      <div className="grid grid-cols-2 gap-8 mt-8">
+        <Button asChild>
+          <Link to="/">Abteilungen</Link>
+        </Button>
+        <Button asChild>
+          <Link to="/">Angebote</Link>
+        </Button>
+        <Button asChild>
+          <Link to="/">Jobs</Link>
+        </Button>
+        <Button asChild>
+          <Link to="/">Mitarbeiter</Link>
+        </Button>
+        <Button asChild>
+          <Link to="/">Partner</Link>
+        </Button>
+      </div>
+    </>
+  );
 }
