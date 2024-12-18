41ecb9bb7952887e663d155fceaa62927af555f1
jayzone91 <register@j4yz0n3.de>
2024-12-18 12:46:54 +0100

    Theme toggle

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,149 +7,155 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import { useNavigate } from "react-router";
+import { ModeToggle } from "./mode-toggle";
 
 export function Navigation() {
   const navigate = useNavigate();
 
   return (
-    <div className="container mx-auto mt-2">
-      <Menubar>
-        <MenubarMenu>
-          <MenubarTrigger onClick={() => navigate("/")}>
-            Startseite
-          </MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Einkauf</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem onClick={() => navigate("/Einkauf/Eingabe")}>
-              Eingabe
-            </MenubarItem>
-            <MenubarItem onClick={() => navigate("/Einkauf")}>
-              Liste
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem onClick={() => navigate("/Einkauf/Abrechnung")}>
-              Abrechnung
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Mitarbeiter</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem onClick={() => navigate("/Mitarbeiter")}>
-              Übersicht
-            </MenubarItem>
-            <MenubarItem onClick={() => navigate("/Mitarbeiter/Geburtstag")}>
-              Geburtstage
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger onClick={() => navigate("/Lieferanten")}>
-            Lieferanten
-          </MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger onClick={() => navigate("/Sage")}>
-            CE Archiv
-          </MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Sage</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem onClick={() => navigate("/Sage/Suche")}>
-              Kundensuche
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem onClick={() => navigate("/Sage/Inventur")}>
-              Inventur
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Wiki</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem onClick={() => navigate("/Wiki")}>
-              Alle Einträge
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem
-              onClick={() =>
-                navigate("/Wiki/412fc476-f341-457d-a06c-c8d27d4cc165")
-              }
-            >
-              Signaturen
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem
-              onClick={() =>
-                navigate("/Wiki/d320c2bf-24ca-43b8-85a3-96fd9c664944")
-              }
-            >
-              Zeiterfassung
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem
-              onClick={() =>
-                navigate("Wiki/89221838-3219-4a51-aecc-9bc45a05f230")
-              }
-            >
-              Datenschutz
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Service</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem onClick={() => navigate("/Service/Seriennummer")}>
-              Seriennummer
-            </MenubarItem>
-            <MenubarItem onClick={() => navigate("/Service/Info")}>
-              Info an Kunde
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>CMS</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem onClick={() => navigate("/CMS")}>
-              Übersicht
-            </MenubarItem>
-            <MenubarItem onClick={() => navigate("/CMS/Abteilungen")}>
-              Abteilungen
-            </MenubarItem>
-            <MenubarItem onClick={() => navigate("/CMS/Mitarbeiter")}>
-              Mitarbeiter
-            </MenubarItem>
-            <MenubarItem onClick={() => navigate("/CMS/Partner")}>
-              Partner
-            </MenubarItem>
-            <MenubarItem onClick={() => navigate("/CMS/Angebote")}>
-              Angebote
-            </MenubarItem>
-            <MenubarItem onClick={() => navigate("/CMS/Jobs")}>
-              Jobs
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger onClick={() => navigate("/Werkstatt")}>
-            Werkstatt
-          </MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>
-            <a
-              href="http://10.0.0.64:8080"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Alter Local Horst
-            </a>
-          </MenubarTrigger>
-        </MenubarMenu>
-      </Menubar>
+    <div className="flex">
+      <div className="container mx-auto mt-2">
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger onClick={() => navigate("/")}>
+              Startseite
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>Einkauf</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem onClick={() => navigate("/Einkauf/Eingabe")}>
+                Eingabe
+              </MenubarItem>
+              <MenubarItem onClick={() => navigate("/Einkauf")}>
+                Liste
+              </MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem onClick={() => navigate("/Einkauf/Abrechnung")}>
+                Abrechnung
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>Mitarbeiter</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem onClick={() => navigate("/Mitarbeiter")}>
+                Übersicht
+              </MenubarItem>
+              <MenubarItem onClick={() => navigate("/Mitarbeiter/Geburtstag")}>
+                Geburtstage
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger onClick={() => navigate("/Lieferanten")}>
+              Lieferanten
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger onClick={() => navigate("/Sage")}>
+              CE Archiv
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>Sage</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem onClick={() => navigate("/Sage/Suche")}>
+                Kundensuche
+              </MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem onClick={() => navigate("/Sage/Inventur")}>
+                Inventur
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>Wiki</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem onClick={() => navigate("/Wiki")}>
+                Alle Einträge
+              </MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem
+                onClick={() =>
+                  navigate("/Wiki/412fc476-f341-457d-a06c-c8d27d4cc165")
+                }
+              >
+                Signaturen
+              </MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem
+                onClick={() =>
+                  navigate("/Wiki/d320c2bf-24ca-43b8-85a3-96fd9c664944")
+                }
+              >
+                Zeiterfassung
+              </MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem
+                onClick={() =>
+                  navigate("Wiki/89221838-3219-4a51-aecc-9bc45a05f230")
+                }
+              >
+                Datenschutz
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>Service</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem onClick={() => navigate("/Service/Seriennummer")}>
+                Seriennummer
+              </MenubarItem>
+              <MenubarItem onClick={() => navigate("/Service/Info")}>
+                Info an Kunde
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>CMS</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem onClick={() => navigate("/CMS")}>
+                Übersicht
+              </MenubarItem>
+              <MenubarItem onClick={() => navigate("/CMS/Abteilungen")}>
+                Abteilungen
+              </MenubarItem>
+              <MenubarItem onClick={() => navigate("/CMS/Mitarbeiter")}>
+                Mitarbeiter
+              </MenubarItem>
+              <MenubarItem onClick={() => navigate("/CMS/Partner")}>
+                Partner
+              </MenubarItem>
+              <MenubarItem onClick={() => navigate("/CMS/Angebote")}>
+                Angebote
+              </MenubarItem>
+              <MenubarItem onClick={() => navigate("/CMS/Jobs")}>
+                Jobs
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger onClick={() => navigate("/Werkstatt")}>
+              Werkstatt
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <a
+                href="http://10.0.0.64:8080"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Alter Local Horst
+              </a>
+            </MenubarTrigger>
+          </MenubarMenu>
+        </Menubar>
+      </div>
+      <div className="mt-2 me-4">
+        <ModeToggle />
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -54,6 +54,7 @@ import Archive from "./Pages/Archive/Archive";
 import NotFound from "./Pages/NotFound";
 import Download from "./Pages/Archive/Download";
 import Login from "./Pages/Login";
+import { ThemeProvider } from "@/components/theme-provider";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -61,111 +62,116 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<RootLayout />}>
-            <Route index element={<Home />} />
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route index element={<Home />} />
 
-            <Route path="Login" element={<Login />} />
+              <Route path="Login" element={<Login />} />
 
-            <Route path="Einkauf">
-              <Route index element={<EinkaufListe />} />
-              <Route path="Eingabe" element={<Auswahl />} />
-              <Route path="Eingabe/:mid" element={<Eingabe />} />
-              <Route path="Abrechnung" element={<Abrechnung />} />
-            </Route>
-
-            <Route path="Mitarbeiter">
-              <Route index element={<MitarbeiterOverview />} />
-              <Route path="Geburtstag" element={<Geburtstage />} />
-              <Route path="Neu" element={<MitarbeiterNeu />} />
-              <Route path=":mid" element={<MitarbeiterDetail />} />
-              <Route
-                path=":mid/Bearbeiten"
-                element={<MitarbeiterBearbeiten />}
-              />
-            </Route>
-
-            <Route path="Lieferanten">
-              <Route index element={<Lieferanten />} />
-              <Route path="Neu" element={<LieferantNeu />} />
-              <Route path=":lid" element={<LieferantenDetails />} />
-              <Route path=":lid/Bearbeiten" element={<LieferantBearbeiten />} />
-              <Route path=":lid/:aid" element={<APDetails />} />
-              <Route
-                path=":lid/:aid/Bearbeiten"
-                element={<AnsprechpartnerBearbeiten />}
-              />
-              <Route path=":lid/Neu" element={<AnsprechpartnerNeu />} />
-            </Route>
-
-            <Route path="Sage">
-              <Route index element={<Archive />} />
-              <Route path=":filename" element={<Download />} />
-              <Route path="Suche" element={<KundenSuche />} />
-
-              <Route path="Inventur">
-                <Route index element={<InventurOverview />} />
-                <Route path=":iyear">
-                  <Route index element={<Jahr />} />
-                  <Route path="Teams" element={<Teams />} />
-                  <Route path="Teams/:iteam" element={<Team />} />
-                </Route>
-              </Route>
-            </Route>
-
-            <Route path="Wiki">
-              <Route index element={<Wikis />} />
-              <Route path="Neu" element={<NewWiki />} />
-              <Route path=":wid" element={<WikiDetails />} />
-              <Route path=":wid/Bearbeiten" element={<EditWiki />} />
-            </Route>
-
-            <Route path="Service">
-              <Route index element={<>Hier gibts nichts zu sehen!</>} />
-              <Route path="Seriennummer" element={<Seriennummer />} />
-              <Route path="Info" element={<InfoPage />} />
-            </Route>
-
-            <Route path="CMS">
-              <Route index element={<CmsOverview />} />
-
-              <Route path="Abteilungen">
-                <Route index element={<AbteilungOverview />} />
-                <Route path="Neu" element={<AbteilungNew />} />
-                <Route path=":aid" element={<AbteilungEdit />} />
+              <Route path="Einkauf">
+                <Route index element={<EinkaufListe />} />
+                <Route path="Eingabe" element={<Auswahl />} />
+                <Route path="Eingabe/:mid" element={<Eingabe />} />
+                <Route path="Abrechnung" element={<Abrechnung />} />
               </Route>
 
               <Route path="Mitarbeiter">
-                <Route index element={<CmsMitarbeiterOverview />} />
-                <Route path="Neu" element={<MitarbeiterNew />} />
-                <Route path=":mid" element={<MitarbeiterEdit />} />
+                <Route index element={<MitarbeiterOverview />} />
+                <Route path="Geburtstag" element={<Geburtstage />} />
+                <Route path="Neu" element={<MitarbeiterNeu />} />
+                <Route path=":mid" element={<MitarbeiterDetail />} />
+                <Route
+                  path=":mid/Bearbeiten"
+                  element={<MitarbeiterBearbeiten />}
+                />
               </Route>
 
-              <Route path="Partner">
-                <Route index element={<PartnerOverview />} />
-                <Route path="Neu" element={<PartnerNew />} />
-                <Route path=":pid" element={<PartnerEdit />} />
+              <Route path="Lieferanten">
+                <Route index element={<Lieferanten />} />
+                <Route path="Neu" element={<LieferantNeu />} />
+                <Route path=":lid" element={<LieferantenDetails />} />
+                <Route
+                  path=":lid/Bearbeiten"
+                  element={<LieferantBearbeiten />}
+                />
+                <Route path=":lid/:aid" element={<APDetails />} />
+                <Route
+                  path=":lid/:aid/Bearbeiten"
+                  element={<AnsprechpartnerBearbeiten />}
+                />
+                <Route path=":lid/Neu" element={<AnsprechpartnerNeu />} />
               </Route>
 
-              <Route path="Angebote">
-                <Route index element={<AngebotOverview />} />
-                <Route path="Neu" element={<AngebotNew />} />
-                <Route path=":aid" element={<AngeboteEdit />} />
+              <Route path="Sage">
+                <Route index element={<Archive />} />
+                <Route path=":filename" element={<Download />} />
+                <Route path="Suche" element={<KundenSuche />} />
+
+                <Route path="Inventur">
+                  <Route index element={<InventurOverview />} />
+                  <Route path=":iyear">
+                    <Route index element={<Jahr />} />
+                    <Route path="Teams" element={<Teams />} />
+                    <Route path="Teams/:iteam" element={<Team />} />
+                  </Route>
+                </Route>
               </Route>
 
-              <Route path="Jobs">
-                <Route index element={<JobsOverview />} />
-                <Route path="Neu" element={<JobsNew />} />
-                <Route path=":jid" element={<JobsEdit />} />
+              <Route path="Wiki">
+                <Route index element={<Wikis />} />
+                <Route path="Neu" element={<NewWiki />} />
+                <Route path=":wid" element={<WikiDetails />} />
+                <Route path=":wid/Bearbeiten" element={<EditWiki />} />
               </Route>
+
+              <Route path="Service">
+                <Route index element={<>Hier gibts nichts zu sehen!</>} />
+                <Route path="Seriennummer" element={<Seriennummer />} />
+                <Route path="Info" element={<InfoPage />} />
+              </Route>
+
+              <Route path="CMS">
+                <Route index element={<CmsOverview />} />
+
+                <Route path="Abteilungen">
+                  <Route index element={<AbteilungOverview />} />
+                  <Route path="Neu" element={<AbteilungNew />} />
+                  <Route path=":aid" element={<AbteilungEdit />} />
+                </Route>
+
+                <Route path="Mitarbeiter">
+                  <Route index element={<CmsMitarbeiterOverview />} />
+                  <Route path="Neu" element={<MitarbeiterNew />} />
+                  <Route path=":mid" element={<MitarbeiterEdit />} />
+                </Route>
+
+                <Route path="Partner">
+                  <Route index element={<PartnerOverview />} />
+                  <Route path="Neu" element={<PartnerNew />} />
+                  <Route path=":pid" element={<PartnerEdit />} />
+                </Route>
+
+                <Route path="Angebote">
+                  <Route index element={<AngebotOverview />} />
+                  <Route path="Neu" element={<AngebotNew />} />
+                  <Route path=":aid" element={<AngeboteEdit />} />
+                </Route>
+
+                <Route path="Jobs">
+                  <Route index element={<JobsOverview />} />
+                  <Route path="Neu" element={<JobsNew />} />
+                  <Route path=":jid" element={<JobsEdit />} />
+                </Route>
+              </Route>
+
+              <Route path="Werkstatt" element={<Werkstatt />} />
             </Route>
-
-            <Route path="Werkstatt" element={<Werkstatt />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
 );
