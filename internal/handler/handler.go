91ff1d4678866a974caba9f0f0202c9b92f3d0ef
jayzone91 <register@j4yz0n3.de>
2025-02-28 15:05:17 +0100

    asd

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,10 +1,24 @@
 package handler
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/computerextra/golang-backend/db"
 )
+
+type Handler struct {
+	logger   *slog.Logger
+	database *db.PrismaClient
+}
+
+func New(logger *slog.Logger, database *db.PrismaClient) *Handler {
+	return &Handler{
+		logger:   logger,
+		database: database,
+	}
+}
 
 func Component(comp templ.Component) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
